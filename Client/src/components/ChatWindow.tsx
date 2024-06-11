5be96fd5fec2257451fcd28938a2f7ae3d6a6e82
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-06-11 23:40:03 +0200

    feat: Add Page Not Found and ChatWindow component.

---
+++ Client/src/components/ChatWindow.tsx
@@ -0,0 +1,72 @@
+export default function ChatWindow() {
+  return (
+    <div className="flex h-[80vh] w-full flex-1 flex-col overflow-y-auto p-2">
+      <ul className="flex-1">
+        <li className="flex w-full justify-start">
+          <p className="m-2 my-6 mr-10 max-w-xl rounded-r-3xl rounded-bl-sm rounded-tl-3xl bg-backgroundMessageReceived p-4 text-primaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex justify-end">
+          <p className="m-2 my-6 ml-10 max-w-xl rounded-l-3xl rounded-br-sm rounded-tr-3xl bg-primaryColor p-4 text-secondaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex w-full justify-start">
+          <p className="m-2 my-6 mr-10 max-w-xl rounded-r-3xl rounded-bl-sm rounded-tl-3xl bg-backgroundMessageReceived p-4 text-primaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex justify-end">
+          <p className="m-2 my-6 ml-10 max-w-xl rounded-l-3xl rounded-br-sm rounded-tr-3xl bg-primaryColor p-4 text-secondaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex w-full justify-start">
+          <p className="m-2 my-6 mr-10 max-w-xl rounded-r-3xl rounded-bl-sm rounded-tl-3xl bg-backgroundMessageReceived p-4 text-primaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex justify-end">
+          <p className="m-2 my-6 ml-10 max-w-xl rounded-l-3xl rounded-br-sm rounded-tr-3xl bg-primaryColor p-4 text-secondaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex w-full justify-start">
+          <p className="m-2 my-6 mr-10 max-w-xl rounded-r-3xl rounded-bl-sm rounded-tl-3xl bg-backgroundMessageReceived p-4 text-primaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+        <li className="flex justify-end">
+          <p className="m-2 my-6 ml-10 max-w-xl rounded-l-3xl rounded-br-sm rounded-tr-3xl bg-primaryColor p-4 text-secondaryTextColor shadow-md shadow-zinc-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            obcaecati labore aliquid maiores ad, neque illum laudantium, odit
+            maxime voluptates ut quasi sequi enim. Enim sed laudantium
+            consequuntur exercitationem aperiam.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
