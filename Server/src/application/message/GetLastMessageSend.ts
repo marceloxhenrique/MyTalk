7579af05b989695e46115e756959b857ab77bbf4
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-07-12 23:39:34 +0200

    feat: Add functionality to retrieve the last message received from a contact.
    - Add functionality in Contact component to display the last message received from a contact
    - Update Message.repository to support retrieval of the last received message
    - Update GetLastMessageSend use case to retrieve the last message sent

--- Server/src/application/message/GetLastMessageSend.ts
+++ Server/src/application/message/GetLastMessageSend.ts
@@ -3,38 +3,22 @@ import { MessageInterface } from "../../domain/message/Message.repository";
 
 export default class GetLastMessageSend {
   constructor(private messageRepo: MessageInterface) {}
-  async execute(senderId: string): Promise<any | null> {
+  async execute(senderId: string): Promise<Message[] | null> {
     if (!senderId) {
       throw new Error("SenderId can not be empty");
     }
     const res = await this.messageRepo.getLastMessage(senderId);
-    // console.log("RES", res);
-    const getLastMessages: LastMessageOutput[] = [];
+    const getLastMessages: Message[] = [];
     if (res) {
       for (let message of res) {
-        const filtered = getLastMessages.filter((item) => item.contact_id === message.contact_id);
-        console.log("Length1", filtered.length);
+        const filtered = getLastMessages.filter(
+          (item) => item.getData().senderId === message.getData().senderId
+        );
         if (filtered.length === 0) {
-          console.log("Length", filtered.length);
           getLastMessages.push(message);
         }
       }
-      console.log("Last Message", getLastMessages);
     }
-    console.log(res);
-    return res;
+    return getLastMessages;
   }
 }
-
-type LastMessageOutput = {
-  message_id: string;
-  message_content: string;
-  messge_user_id: string;
-  message_contact_id: string;
-  message_sent_at: string;
-  contact_id: string;
-  contact_contact_id: string;
-  contact_email: string;
-  contact_contact_name: string;
-  contact_user_id: string;
-};
