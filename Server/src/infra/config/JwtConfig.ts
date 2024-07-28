0905cbe04c48180032fc0996e5092c2ca0e9c55a
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-07-28 04:16:49 +0200

    feat: Add a vercel.json file

--- Server/src/infra/config/JwtConfig.ts
+++ Server/src/infra/config/JwtConfig.ts
@@ -6,8 +6,8 @@ export default interface JwtConfig {
 }
 
 export const JwtConfig: JwtConfig = {
-  tokenSecret: process.env.JWT_TOKEN_SECRET || "MySuperSecret",
-  tokenExpiresIn: process.env.JWT_TOKEN_EXPIRATION || "1h",
-  refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET || "MySuperSecret1",
-  refreshTokenExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION || "24h",
+  tokenSecret: process.env.JWT_TOKEN_SECRET ?? "MySuperSecret",
+  tokenExpiresIn: process.env.JWT_TOKEN_EXPIRATION ?? "1h",
+  refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET ?? "MySuperSecret1",
+  refreshTokenExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION ?? "24h",
 };
